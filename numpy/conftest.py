82b4bc9c99d9174274a071cddb3e143cfddf562d
Rohan Yadav <rohany@alumni.cmu.edu>
2021-03-08 10:36:13 -0800

    numpy: adjust benchmark runner to perform a warmup iteration

--- numpy/conftest.py
+++ numpy/conftest.py
@@ -3,9 +3,7 @@ import pytest
 def tacoBench(benchmark):
     def f(func):
         # Take statistics based on 10 rounds.
-        benchmark.pedantic(func, rounds=10, iterations=1)
-        # How do i set please use 10 rounds...
-        # benchmark(func)
+        benchmark.pedantic(func, rounds=10, iterations=1, warmup_rounds=1)
     return f
 
 def pytest_addoption(parser):
