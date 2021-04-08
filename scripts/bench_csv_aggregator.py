55dcf1a94c700cd35a29995a0dbdf9c0fd4ad457
Olivia W Hsu <owhsu@lanka.csail.mit.edu>
2021-04-08 16:23:08 -0400

    Add rename csv aggregator and add in --taco flag to work for both taco and numpy

--- scripts/bench_csv_aggregator.py
+++ scripts/bench_csv_aggregator.py
@@ -5,7 +5,7 @@ import sys
 import tqdm
 import argparse
 
-def aggregateTacoBenches(folder, outfile, labelSet=None):
+def aggregateTacoBenches(folder, outfile, taco=False, labelSet=None):
     first = True
     outputFile = open(outfile, 'w+')
     writer = csv.writer(outputFile, delimiter=',')
@@ -20,8 +20,9 @@ def aggregateTacoBenches(folder, outfile, labelSet=None):
             with open(fname, 'r') as f:
                 # Discard the first 10 lines. This corresponds to the
                 # google-benchmark generated header.
-                # for i in range(0, 10):
-                #     f.readline()
+                if taco:
+                    for i in range(0, 10):
+                        f.readline()
                 # Open the rest of the file as a CSV.
                 reader = csv.reader(f)
                 # Attempt to read the header from CSV. If this fails,
@@ -52,5 +53,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('target_directory', type=str, help="Directory containing CSV's to aggregate")
 parser.add_argument('output_csv_name', type=str, help="Name of the CSV to generate")
 parser.add_argument('--label_set_file', type=str, default=None, help='Set to output all valid labels seen to a file')
+parser.add_argument('--taco', action='store_true', help='Flag to aggregate TACO csvs, default is numpy')
 args = parser.parse_args()
-aggregateTacoBenches(args.target_directory, args.output_csv_name, labelSet=args.label_set_file)
+aggregateTacoBenches(args.target_directory, args.output_csv_name, taco=args.taco, labelSet=args.label_set_file)
